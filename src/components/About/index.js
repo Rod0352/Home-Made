e4c57e128a023ba7c5c40001a79034b4bf6bd7ba
Rod0352 <92071426+Rod0352@users.noreply.github.com>
2022-04-06 14:09:16 -0400

    link inn footer functional, opens new tab

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,26 +3,26 @@ import React from "react";
 function About() {
     return (
         <div >
-                    <h1 className="card-title text-center aboutTitle">About</h1>
-        <div className="card mb-3 about" >
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img
-                        src={require(`../../assets/About/AboutImg.jpg`)}
-                        alt="About"
-                    />    </div>
-                <div className="col-md-8">
-                    <div className="card-body text-center">
-                        <br></br>
-                        <br></br>
-                        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
+            <h1 className="card-title text-center aboutTitle">About</h1>
+            <div className="card mb-3 about" >
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img
+                            src={require(`../../assets/About/AboutImg.jpg`)}
+                            alt="About"
+                        />    </div>
+                    <div className="col-md-8">
+                        <div className="card-body text-center">
+                            <br></br>
+                            <br></br>
+                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        </div>
-    );        
+    );
 }
 
 export default About;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,8 +6,8 @@ function Footer() {
             <div className="py-5 text-center">
                 <div className="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top">
                     <div className="col-md-4 d-flex justify-content-center align-items-center">
-                        <h1 className="text-warning titleFooter">Click here to Support the BLM Movement!
-                        </h1>
+                        <a href="https://blacklives.help/?gclid=CjwKCAjw9LSSBhBsEiwAKtf0n-H3IcuDDdPpqvavzPRe0oHh4dCJBb7q4K4o5Cmsshuy69TVLvgSehoC4Y0QAvD_BwE" target="blank" className="text-warning titleFooter">Click here to Support the BLM Movement!
+                        </a>
                     </div>
                 </div>
             </div>
